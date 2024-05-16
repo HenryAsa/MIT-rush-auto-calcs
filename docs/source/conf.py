40e3ea1046d99199692d4dc22fe816caa3c3ec90
Henry Asa <henryjacobasa@gmail.com>
2024-05-15 22:12:57 -0400

    Added `sphinx` to the repository

---
+++ docs/source/conf.py
@@ -0,0 +1,94 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'MIT Rush Auto Analysis'
+copyright = '2024, Henry Asa'
+author = 'Henry Asa'
+release = '0.0.0'
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../..'))
+print()
+
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+# extensions = [
+#     'sphinx.ext.autodoc',
+#     'sphinx.ext.coverage',
+#     'sphinx.ext.napoleon',
+# ]
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.coverage',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.todo',
+    'sphinx.ext.graphviz',
+    'sphinx.ext.ifconfig',
+    'numpydoc',
+    'sphinx_design',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+# -----------------------------------------------------------------------------
+# Intersphinx configuration
+# -----------------------------------------------------------------------------
+intersphinx_mapping = {
+    'neps': ('https://numpy.org/neps', None),
+    'python': ('https://docs.python.org/3', None),
+    'scipy': ('https://docs.scipy.org/doc/scipy', None),
+    'matplotlib': ('https://matplotlib.org/stable', None),
+    'imageio': ('https://imageio.readthedocs.io/en/stable', None),
+    'skimage': ('https://scikit-image.org/docs/stable', None),
+    'pandas': ('https://pandas.pydata.org/pandas-docs/stable', None),
+    'scipy-lecture-notes': ('https://scipy-lectures.org', None),
+    'pytest': ('https://docs.pytest.org/en/stable', None),
+    'numpy-tutorials': ('https://numpy.org/numpy-tutorials', None),
+    'numpydoc': ('https://numpydoc.readthedocs.io/en/latest', None),
+    'dlpack': ('https://dmlc.github.io/dlpack/latest', None)
+}
+
+# -----------------------------------------------------------------------------
+# Autosummary
+# -----------------------------------------------------------------------------
+autosummary_generate = True             # Turn on sphinx.ext.autosummary
+autoclass_content = "both"              # Add __init__ doc (ie. params) to class summaries
+html_show_sourcelink = False            # Remove 'view source code' from top of page (for html, not python)
+autodoc_inherit_docstrings = True       # If no docstring, inherit from base class
+set_type_checking_flag = True           # Enable 'expensive' imports for sphinx_autodoc_typehints
+nbsphinx_allow_errors = True            # Continue through Jupyter errors
+# autodoc_typehints = "description"       # Sphinx-native method. Not as good as sphinx_autodoc_typehints
+add_module_names = False                # Remove namespaces from class/method signatures
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# html_theme = 'alabaster'
+html_theme = 'pydata_sphinx_theme'
+# html_static_path = ['_static']
+
+
+autosummary_imported_members = True
+
+
+html_theme_options = {
+    "github_url": "https://github.com/HenryAsa/MIT-rush-auto-calcs",
+}
---
+++ docs/source/index.rst
@@ -0,0 +1,21 @@
+Welcome to MIT Rush Auto Analysis's documentation!
+==================================================
+
+.. autosummary:: 
+   :toctree: _autosummary
+   :recursive:
+
+   race_analysis
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
